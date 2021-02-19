f27a2a7489916a87fa9923697ea372387e24f211
samuel-velez <samuelvelez@outlook.com>
2021-02-18 17:13:33 -0800

    added a plot

--- Date_range_Wrapper.R
+++ Date_range_Wrapper.R
@@ -10,7 +10,6 @@ statcan_daterange<-function(vectors, start_time, end_time=Sys.time(), use_ref_da
   url = "https://www150.statcan.gc.ca/t1/wds/rest/getBulkVectorDataByRange"
   start_date = paste0(start_time,"T00:00")
   end_date = paste0(end_time,"T00:00")
-  #vectors = c("v41690973")
   
   vectors=gsub("^v","",vectors)
   vectors_string=paste0('"vectorIds":[',paste(map(as.character(vectors),function(x)paste0('"',x,'"')),collapse = ", "),"]")
@@ -21,10 +20,13 @@ statcan_daterange<-function(vectors, start_time, end_time=Sys.time(), use_ref_da
   r <- POST(url, body=body, encode = "json", timeout(200), add_headers("Content-Type"="application/json"))
   result <- content(r, as = "text", encoding = "UTF-8")
   df <- fromJSON(result, flatten = TRUE)
-  df$object.vectorDataPoint[[1]][c("refPer","value")]
-  names(df)[1] <- "Date"
-  names(df)[2] <- "Value"
+  df <- df$object.vectorDataPoint[[1]][c("refPer","value")]
   }
 
-df <- statcan_daterange("v1", "2019-01-01", "2021-02-18")
-df
+library(ggplot2)
+
+df <- statcan_daterange("v1", start_time = "2019-01-01", end_time = "2021-02-18")
+
+df$refPer <- as.Date(df$refPer)
+ggplot(df, aes(x=refPer, y=value)) +
+  geom_line()
